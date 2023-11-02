4e295f5f48f01e5e36db135759df2d5fbd144cef
OatileM <osmdlela@gmail.com>
2023-11-02 10:08:46 +0200

    first commit

---
+++ Controllers/SassaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebScrapper.Controllers
+{
+    [Route("api/sassa")]
+    [ApiController]
+    public class SassaController: ControllerBase
+    {
+
+        private SassaScraper _sassaScraper = new SassaScraper();
+
+        [HttpGet]
+        [Route("scrape")]
+        public IActionResult ScrapeSassaData()
+        {
+            try
+            {
+                string scrapedData = _sassaScraper.ScrapeSassaData();
+
+                if (scrapedData != "Data not found")
+                {
+                    return Ok(scrapedData); // Return the scraped data in the response.
+                }
+                else
+                {
+                    return NotFound("Data not found"); // Handle the case where the data is not found.
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal Server Error: " + ex.Message); // Handle exceptions.
+            }
+        }
+    }
+}
---
+++ SassaScraper.cs
@@ -0,0 +1,64 @@
+﻿using HtmlAgilityPack;
+
+namespace WebScrapper
+{
+    public class SassaScraper
+    {
+        private string baseUrl = "https://www.sassa.gov.za";
+
+        public SassaScraper()
+        {
+            // Constructor, initialize any necessary objects or settings.
+        }
+
+        public string ScrapeSassaData()
+        {
+            // Uses HtmlAgilityPack to load and parse web pages.
+            // Extracts data from the SASSA website.
+            // Return the scraped data in the desired format.
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = baseUrl + "/SitePages/Supplier-Registration-Forms.aspx";
+                    string htmlContent = client.GetStringAsync(url).Result;
+
+                    // Load the HTML content into an HtmlDocument for parsing.
+                    var htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(htmlContent);
+
+                    // Use XPath to select specific elements or data from the HTML.
+                    var dataNode = htmlDocument.DocumentNode.SelectSingleNode("//*[@id=\"cbqwpctl00_ctl36_g_2ed11b4d_7320_4753_b7b0_ca0c83357450\"]");
+
+                    if (dataNode != null)
+                    {
+                        string extractedData = dataNode.InnerText;
+
+                        // Format the extracted data as needed. For example, converting it to JSON.
+                        var formattedData = new
+                        {
+                            Data = extractedData
+                        };
+
+                        // Serialize the formatted data to JSON.
+                        string jsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(formattedData);
+
+                        return jsonResult;
+                    }
+                    else
+                    {
+                        return "Data not found"; // Handles the case where the data is not found on the page.
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handles exceptions, e.g., network issues, invalid URLs, or HTML parsing errors.
+                Console.WriteLine("Error: " + ex.Message);
+                return "Error occurred during scraping";
+            }
+
+        }
+
+    }
+}
